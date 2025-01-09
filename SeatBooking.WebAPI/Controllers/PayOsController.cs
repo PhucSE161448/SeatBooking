614f70b1064c8631d5ce1b89083e66ea5b6d5941
Phúcc <112921620+PhucSE161448@users.noreply.github.com>
2025-01-09 17:16:32 +0700

    update code

--- SeatBooking.WebAPI/Controllers/PayOsController.cs
+++ SeatBooking.WebAPI/Controllers/PayOsController.cs
@@ -66,7 +66,7 @@ namespace SeatBooking.WebAPI.Controllers
             }
           
             // {ApiEndPointConstant.UserCourse.CourseUserEndpointJoin}?userId={userId}&courseId={courseId}&paymentMethod={paymentMethod}&fee={fee}&fullName={fullName}&phoneNumber={phoneNumber}"
-            var successUrl = $"https://seat-booking.azurewebsites.net/api/PayOs/Success?id={numbersString}&amount={paymentRequest.TotalAmount}&showTime={paymentRequest.BookingShow}";
+            var successUrl = $"https://seat-booking.azurewebsites.net/api/PayOs?id={numbersString}&amount={paymentRequest.TotalAmount}&showTime={paymentRequest.BookingShow}";
             var cancelUrl = "https://seat-booking-drab.vercel.app/";
             PaymentData paymentData = new PaymentData(orderCode, (int)paymentRequest.TotalAmount, $"thanh toan ghe ngoi dot {paymentRequest.BookingShow}", items, cancelUrl, successUrl);
             CreatePaymentResult createPayment = await payOs.createPaymentLink(paymentData);
